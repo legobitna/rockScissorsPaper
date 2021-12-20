2b6d092731f0194fdbf6a98f46c4a3d1bc9958ee
Bitna Kim <tallshe108@hanmail.net>
2021-12-20 19:51:41 +0900

    redesign game

--- src/App.js
+++ src/App.js
@@ -1,90 +1,77 @@
-import React,{useState} from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import ChoiceCard from "./components/ChoiceCard";
-import NavBar from "./components/NavBar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CHOICES = {
   scissors: {
     name: "scissors",
-    url: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+    url: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png",
   },
   paper: {
     name: "paper",
-    url: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png"
+    url: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
   },
   rock: {
     name: "rock",
-    url:
-      "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png"
-  }
-}
+    url: "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  },
+};
 
+const judgement = (uChoice, cChoice) => {
+  if (uChoice === cChoice) return "TIE";
+  else if (uChoice === "rock") return cChoice === "scissors" ? "WIN" : "LOSE";
+  else if (uChoice === "scissors") return cChoice === "paper" ? "WIN" : "LOSE";
+  else if (uChoice === "paper") return cChoice === "rock" ? "WIN" : "LOSE";
+};
 
-
-const judgement = (uChoice, cChoice) =>{
-    if(uChoice === cChoice) return 'TIE';
-    else if (uChoice==='rock') return cChoice === 'scissors'?'WIN':'LOSE';
-    else if (uChoice==='scissors') return cChoice === 'paper'?'WIN':'LOSE';
-    else if (uChoice==='paper') return cChoice === 'rock'?'WIN':'LOSE';
-    return 'dd';
-}
-
-const randomChoice= () =>{
+const randomChoice = () => {
   const keys = Object.keys(CHOICES);
-  const randomNum = Math.floor(Math.random()*keys.length);
+  const randomNum = Math.floor(Math.random() * keys.length);
   const randomName = keys[randomNum];
 
- 
   return CHOICES[randomName];
-}
+};
 
-const addHistory=(value)=>{
-  return <li>{value}</li>
-}
 function App() {
-  
-const [userChoice, setUserChoice] = useState(null);
-const [computerChoice, setComputerChoice] = useState(null);
-const [result, setResult]=useState(null);
-const [history,setHistory]=useState([]);
+  const [userChoice, setUserChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+  const [result, setResult] = useState(null);
 
-
-const onplay =(value) =>{
-  setUserChoice(CHOICES[value]);
-  let a = randomChoice()
-  setComputerChoice(a);
-  let resultShow = judgement(CHOICES[value].name,a.name)
-  setResult(resultShow);
-  history.push(addHistory(resultShow))
-  setHistory(history)
-  
-}
-
-console.log(userChoice,computerChoice,result);
+  const onplay = (value) => {
+    setUserChoice(CHOICES[value]);
+    let a = randomChoice();
+    setComputerChoice(a);
+    let resultShow = judgement(CHOICES[value].name, a.name);
+    setResult(resultShow);
+  };
 
   return (
-    <div >
-     <NavBar></NavBar>
-    <div className="board">
-      <ChoiceCard
-      title ="you"
-      winner={result}
-      imgURL={userChoice && userChoice.url} />
-      <ChoiceCard
-       title ="computer"
-       winner={result}
-       imgURL={computerChoice && computerChoice.url} />
-       <ul id="history">
-         {history}
-       </ul>
+    <div>
+      <div className="board">
+        <ChoiceCard
+          title="you"
+          winner={result}
+          imgURL={userChoice && userChoice.url}
+        />
+        <ChoiceCard
+          title="computer"
+          winner={result}
+          imgURL={computerChoice && computerChoice.url}
+        />
+      </div>
+      <div className="text-center">
+        <i
+          class="fa fa-hand-scissors fa-2x"
+          onClick={() => onplay("scissors")}
+        ></i>
+        <i class="fa fa-hand-rock fa-2x" onClick={() => onplay("rock")}></i>
+        <i class="fa fa-hand-paper fa-2x" onClick={() => onplay("paper")}></i>
+      </div>
+      <div class="text-center">
+        <h1>{result}</h1>
+      </div>
     </div>
-    <button onClick={()=> onplay('rock')}>Rock</button>
-    <button onClick={()=> onplay('scissors')}>scissors</button>
-    <button onClick={()=> onplay('paper')}>paper</button>
-    </div>
-    
   );
 }
 
--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function ChoiceCard(props) {
-    let result ;
-    if(props.title ==='computer'){
-        result =  props.winner ==='TIE'?'tie':props.winner ==='WIN'?'loser':'winner'
-    }else {
-        result = props.winner ==='TIE'?'tie':props.winner ==='WIN'?'winner':'loser'
-
-    }
-    return (
-        <div className={`choice-card ${result}`}>
-            <h1>{props.title}</h1>
-            <img src={props.imgURL}/>
-            <h3>{result.toUpperCase()}</h3>
-        </div>
-    )
+  let result;
+  if (props.title === "computer") {
+    result =
+      props.winner === "WIN"
+        ? "loser"
+        : props.winner === "LOSE"
+        ? "winner"
+        : "tie";
+  } else {
+    result =
+      props.winner === "WIN"
+        ? "winner"
+        : props.winner === "LOSE"
+        ? "loser"
+        : "tie";
+  }
+  return (
+    <div className={`choice-card ${result}`}>
+      <h1>{props.title}</h1>
+      <img src={props.imgURL} />
+      <h3>{result.toUpperCase()}</h3>
+    </div>
+  );
 }
